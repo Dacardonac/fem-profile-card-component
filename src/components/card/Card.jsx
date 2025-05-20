f9ed5ce5894fe997a6a1b7971fb535837e9d2c67
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-05-20 11:26:28 -0500

    feat: add Card component with styles for user profile display

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,34 @@
+import styles from './Card.module.scss';
+
+const Card = () => {
+  return (
+    <article className={styles.card}>
+      <header className={styles['card__header']}></header>
+      <figure className={styles['card__avatar']}>
+        <img
+          className={styles['card__avatar-img']}
+          src="/src/images/image-victor.webp"
+          alt="Victor Avatar Photo"
+        />
+      </figure>
+      <h1 className={styles['card__username']}>
+        Victor Crest <span className={styles['card__username-age']}>26</span>
+      </h1>
+      <h2 className={styles['card__city']}>London</h2>
+      <hr className={styles['card__separation']} />
+      <section className={styles['card__stats']}>
+        <div className={styles['card__stats-divs']}>
+          80K <span className={styles['card__stats--light']}>Followers</span>
+        </div>
+        <div className={styles['card__stats-divs']}>
+          803K <span className={styles['card__stats--light']}>Likes</span>
+        </div>
+        <div className={styles['card__stats-divs']}>
+          1.4K <span className={styles['card__stats--light']}>Photos</span>
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default Card;
