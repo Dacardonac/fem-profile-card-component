b553aca84f4af015e4392eb60be17ce8a6c476c5
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-05-20 11:26:15 -0500

    chore: initialize project with React, Vite, and essential dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import app from './App.module.scss'
+import Card from './components/card/Card'
+
+function App() {
+
+  return (
+    <div className={app.App}>
+      <Card />
+    </div>
+  )
+}
+
+export default App
